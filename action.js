1a54ee98d36fc25f3e40883e6c3fb61803015a1b
Chandni27 <44206924+Chandni27@users.noreply.github.com>
2020-06-03 15:56:13 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,6 +1,7 @@
 function redirect()
 {
     var looting= "Looting information here"
-    window.location.href = "mailto:?to=info@santamonica.gov&subject=Looting"; 
+    window.location.href = "mailto:?to=info@santamonica.gov&subject="+ looting+"";  
 }
+
 <body onload="javascript: redirect();">
