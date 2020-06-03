dc70fcf5cf5f23116228ba8158914ba81faa915e
Chandni27 <44206924+Chandni27@users.noreply.github.com>
2020-06-03 16:17:39 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,6 +1,20 @@
 function redirect()
 {
-    var looting= "Looting information here"
+    var x = new Object();
+    
+    
+    x[1] = "Looting information here";
+    x[2] = "Info on looters";
+    x[3] = "Looters on video";
+    x[4] = "Santa monica looters";
+    x[5] = "looters tip id";
+    x[6] = "id on looters in video";
+    x[7] = "Value";
+    x[8] = "Value";
+    x[9] = "Value";
+    x[10] = "Value";
+    
+    var looting= x.get(Math.floor((Math.random() * 10) + 1);
     window.location.href = "mailto:?to=info@santamonica.gov&subject="+ looting+"";  
 }
 
